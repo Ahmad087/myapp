46a50def611249485d83a63797a53118b342e7b2
Ahmad Salim <yanz@Ahmads-MacBook-Air.local>
2021-05-28 11:18:48 +0200

    i test get post and delete using postman and it worked only issue for now is that button on localhost:3000/patientlist.html

---
+++ client/public/javascript/models/patient.js
@@ -0,0 +1,9 @@
+function Patient(f_name, l_name, location, dob, illness) {
+
+   
+    this.patient_name = f_name;
+    this.patient_l_name = l_name;
+    this.patient_location = location;
+    this.patient_dob = dob;
+    this.patient_illness = illness;
+}
